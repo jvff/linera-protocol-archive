933c192a6a0207c8054978b2900be258d6502a49
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-05-18 00:48:36 +0000

    Derive `Clone` for `SerializedMessage`
    Allow it to be cloned so that the same message can be sent to multiple
    servers.

--- zef-base/src/serialize.rs
+++ zef-base/src/serialize.rs
@@ -8,7 +8,7 @@ use crate::error::*;
 use failure::format_err;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Clone, Serialize, Deserialize, Debug)]
 pub enum SerializedMessage {
     // Inbound
     BlockProposal(Box<BlockProposal>),
