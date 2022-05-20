165668e19c2b2dc4d4d53ccf7ed11dec2d6fecfc
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-05-20 17:16:03 +0000

    Remove `{de,}serialize_message` helper functions
    All serialization is now handled by the `Codec` type.

--- zef-base/src/serialize.rs
+++ zef-base/src/serialize.rs
@@ -5,7 +5,6 @@
 use super::messages::*;
 use crate::error::*;
 
-use failure::format_err;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
@@ -20,27 +19,6 @@ pub enum SerializedMessage {
     Error(Box<Error>),
     // Internal to a validator
     CrossChainRequest(Box<CrossChainRequest>),
-}
-
-fn serialize<T>(msg: &T) -> Vec<u8>
-where
-    T: Serialize,
-{
-    let mut buf = Vec::new();
-    bincode::serialize_into(&mut buf, msg)
-        .expect("Serializing to a resizable buffer should not fail.");
-    buf
-}
-
-pub fn serialize_message(msg: &SerializedMessage) -> Vec<u8> {
-    serialize(msg)
-}
-
-pub fn deserialize_message<R>(reader: R) -> Result<SerializedMessage, failure::Error>
-where
-    R: std::io::Read,
-{
-    bincode::deserialize_from(reader).map_err(|err| format_err!("{}", err))
 }
 
 impl From<BlockProposal> for SerializedMessage {
