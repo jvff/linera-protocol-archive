058caa546b505d029d028d371e498e550379073d
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-05-24 18:45:03 +0000

    Create a custom `LengthDelimitedCodec`
    Wraps another codec, so that the outer codec handles framing and the
    inner codec handles serialization. The main goal of this configuration
    is to increase performance by only attempting to deserialize messages if
    the codec knows that all of the necessary bytes are available. Encoding
    is also zero-copy, because the serializer writes directly to the buffer
    used by the outer codec.

--- zef-service/src/codec.rs
+++ zef-service/src/codec.rs
@@ -1,22 +1,101 @@
 use bytes::{Buf, BufMut, BytesMut};
-use std::io;
+use std::{io, mem, ops::DerefMut};
 use thiserror::Error;
 use tokio_util::codec::{Decoder, Encoder};
 use zef_base::rpc;
 
 /// An encoder/decoder of [`rpc::Message`]s for the RPC protocol.
-pub type Codec = BincodeCodec;
+pub type Codec = LengthDelimitedCodec<BincodeCodec>;
+
+/// An encoder/decoder of length delimited frames.
+///
+/// The frames are then processed by the `InnerCodec`.
+pub struct LengthDelimitedCodec<InnerCodec> {
+    inner: InnerCodec,
+}
+
+impl LengthDelimitedCodec<BincodeCodec> {
+    /// Create a new instance of the encoder/decoder.
+    pub fn new() -> Self {
+        LengthDelimitedCodec {
+            inner: BincodeCodec,
+        }
+    }
+}
+
+impl<InnerCodec, Message> Encoder<Message> for LengthDelimitedCodec<InnerCodec>
+where
+    InnerCodec: Encoder<Message>,
+    Error: From<InnerCodec::Error>,
+{
+    type Error = Error;
+
+    fn encode(&mut self, message: Message, buffer: &mut BytesMut) -> Result<(), Self::Error> {
+        let prefix_size = mem::size_of::<u32>();
+
+        buffer.put_u32(0);
+
+        self.inner.encode(message, buffer)?;
+
+        let frame_size = buffer.len();
+        let payload_size = frame_size - prefix_size;
+
+        let mut start_of_buffer = buffer.deref_mut();
+
+        start_of_buffer.put_u32(
+            payload_size
+                .try_into()
+                .map_err(|_| Error::MessageTooBig { size: payload_size })?,
+        );
+
+        Ok(())
+    }
+}
+
+impl<InnerCodec> Decoder for LengthDelimitedCodec<InnerCodec>
+where
+    InnerCodec: Decoder,
+    Error: From<InnerCodec::Error>,
+{
+    type Item = InnerCodec::Item;
+    type Error = Error;
+
+    fn decode(&mut self, buffer: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        let prefix_size = mem::size_of::<u32>();
+
+        if buffer.len() < prefix_size {
+            return Ok(None);
+        }
+
+        let payload_size = u32::from_be_bytes(
+            buffer[..prefix_size]
+                .try_into()
+                .expect("Incorrect prefix size to select size bytes"),
+        );
+
+        let frame_size =
+            u32::try_from(prefix_size).expect("Prefix size is too large") + payload_size;
+
+        if buffer.len().try_into().unwrap_or(u32::MAX) < frame_size {
+            buffer.reserve(frame_size.try_into().expect("u32 should fit in a usize"));
+            return Ok(None);
+        }
+
+        let _prefix = buffer.split_to(prefix_size);
+        let mut payload =
+            buffer.split_to(payload_size.try_into().expect("u32 should fit in a usize"));
+
+        match self.inner.decode(&mut payload) {
+            Ok(Some(message)) => Ok(Some(message)),
+            Ok(None) => Err(Error::FrameWithIncompleteMessage),
+            Err(error) => Err(error.into()),
+        }
+    }
+}
 
 /// The encoder/decoder of [`rpc::Message`]s that handles the serialization of messages.
 #[derive(Clone, Copy, Debug)]
 pub struct BincodeCodec;
-
-impl BincodeCodec {
-    /// Create a new instance of the encoder/decoder.
-    pub fn new() -> Self {
-        BincodeCodec
-    }
-}
 
 impl Encoder<rpc::Message> for BincodeCodec {
     type Error = Error;
@@ -58,4 +137,12 @@ pub enum Error {
 
     #[error("Failed to serialize outgoing message")]
     Serialization(#[source] bincode::ErrorKind),
+
+    #[error("Message is too big to fit in a protocol frame: \
+        message is {size} bytes but can't be larger than {max} bytes.",
+        max = u32::MAX)]
+    MessageTooBig { size: usize },
+
+    #[error("Frame contains an incomplete message")]
+    FrameWithIncompleteMessage,
 }
