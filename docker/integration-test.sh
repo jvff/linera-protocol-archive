1debd061eedc7e22a6e179ecde46c0308962581e
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-06-01 22:27:20 +0000

    Use Docker Compose to run the integration test
    Split the `integration-test.sh` script into four scripts. The
    `integration-test.sh` now only generates a `docker-compose.yml` file and
    uses it to start the test. The `setup.sh` script is used by a "setup"
    container to generate the necessary files, the `run-client.sh` script is
    used by a "client" container to run all of the client operations, and
    the `run-server.sh` script is used to start a server.
    
    The new script allows specifying the number of validators and shards to
    make it easy to run different test runs.

---
+++ docker/integration-test.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+NUM_VALIDATORS="$1"
+NUM_SHARDS="$2"
+
+if [ -z "$NUM_VALIDATORS" ] || [ -z "$NUM_SHARDS" ]; then
+    echo "USAGE: ./integration-test.sh NUM_VALIDATORS NUM_SHARDS" >&2
+    exit 1
+fi
+
+# Generate one volume for each validator's configuration files
+server_volumes() {
+    for server in $(seq 1 ${NUM_VALIDATORS}); do
+        echo "  server_${server}:"
+    done
+}
+
+# Generate server volume mounts for the setup container
+server_volumes_for_setup() {
+    for server in $(seq 1 ${NUM_VALIDATORS}); do
+        echo "      - server_${server}:/config/server_${server}"
+    done
+}
+
+# Generate one service for each validator
+server_services() {
+    for server in $(seq 1 ${NUM_VALIDATORS}); do
+        cat << EOF
+  server_${server}:
+    build: .
+    command: ./run-server.sh ${NUM_SHARDS}
+    volumes:
+      - common:/config/common:ro
+      - server_${server}:/config/server:ro
+    depends_on:
+      - setup
+EOF
+    done
+}
+
+# Generate final Docker Compose configuration
+cat > docker-compose.yml << EOF
+volumes:
+  common:
+  client:
+$(server_volumes)
+
+services:
+  setup:
+    build: .
+    command: ./setup.sh ${NUM_VALIDATORS} ${NUM_SHARDS}
+    volumes:
+      - common:/config/common
+      - client:/config/client
+$(server_volumes_for_setup)
+$(server_services)
+  client:
+    build: .
+    command: ./run-client.sh
+    volumes:
+      - common:/config/common:ro
+      - client:/config/client
+    depends_on:
+      - setup
+EOF
+
+docker compose up
