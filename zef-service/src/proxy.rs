60a33ec1c886e752466ca1ddffcc61600b35d0ff
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-06-14 14:58:31 +0000

    WIP Implement UDP proxy

--- zef-service/src/proxy.rs
+++ zef-service/src/proxy.rs
@@ -1,9 +1,15 @@
 use anyhow::{anyhow, bail, Result};
 use futures::{SinkExt, StreamExt};
-use std::{net::Ipv4Addr, path::PathBuf};
+use std::{
+    net::{Ipv4Addr, SocketAddr},
+    path::PathBuf,
+};
 use structopt::StructOpt;
-use tokio::net::{TcpListener, TcpStream};
-use tokio_util::codec::Framed;
+use tokio::{
+    net::{TcpListener, TcpStream, UdpSocket},
+    sync::mpsc,
+};
+use tokio_util::{codec::Framed, udp::UdpFramed};
 use zef_base::rpc;
 use zef_service::{
     codec::Codec,
@@ -33,7 +39,7 @@ async fn main() -> Result<()> {
 
     match config.validator.network.protocol {
         NetworkProtocol::Tcp => run_tcp_proxy(config.validator.network).await,
-        NetworkProtocol::Udp => todo!(),
+        NetworkProtocol::Udp => run_udp_proxy(config.validator.network).await,
     }
 }
 
@@ -64,7 +70,7 @@ async fn proxy_tcp_connection(connection: TcpStream, config: ValidatorNetworkCon
         .ok_or_else(|| anyhow!("Disconnection before a request was received"))??;
 
     let shard = select_shard_for(&request, &config)?;
-    let mut backend_transport = connect_to_shard(shard).await?;
+    let mut backend_transport = connect_to_shard_with_tcp(shard).await?;
 
     backend_transport.send(request).await?;
     let response = backend_transport
@@ -96,8 +102,72 @@ fn select_shard_for<'s>(
     Ok(config.get_shard_for(chain_id))
 }
 
-async fn connect_to_shard(shard: &ShardConfig) -> Result<Framed<TcpStream, Codec>> {
+async fn connect_to_shard_with_tcp(shard: &ShardConfig) -> Result<Framed<TcpStream, Codec>> {
     let connection = TcpStream::connect((&*shard.host, shard.port)).await?;
 
     Ok(Framed::new(connection, Codec))
+}
+
+async fn run_udp_proxy(config: ValidatorNetworkConfig) -> Result<()> {
+    let socket = UdpSocket::bind((Ipv4Addr::new(0, 0, 0, 0), config.port)).await?;
+    let (response_sender, response_receiver) = mpsc::channel(10);
+    let mut transport = UdpFramed::new(socket, Codec);
+
+    loop {
+        tokio::select! {
+            result = transport.next().await => match result {
+                Ok((message, sender_address)) => spawn_udp_request_proxy(
+                    sender_address,
+                    message,
+                    response_sender.clone(),
+                    config.clone(),
+                ),
+                Err(error) => log::debug!("Ignoring failed incoming request. Error: {error}"),
+            },
+            response = response_receiver.recv().await => {
+                let response = response.expect("Internal channel should never be closed");
+
+                if let Err(error) = transport.send(response).await {
+                    log::debug!("Failed to send response: {error}");
+                }
+            }
+        }
+    }
+}
+
+fn spawn_udp_request_proxy(
+    sender_address: SocketAddr,
+    request: rpc::Message,
+    response_sender: mpsc::Sender<()>,
+    config: ValidatorNetworkConfig,
+) {
+    tokio::spawn(async move {
+        if let Err(error) =
+            proxy_udp_request(sender_address, request, config, response_sender).await
+        {
+            log::debug!("Error proxying request: {error}");
+        }
+    });
+}
+
+async fn proxy_udp_request(
+    sender_address: SocketAddr,
+    request: rpc::Message,
+    response_sender: mpsc::Sender<()>,
+    config: ValidatorNetworkConfig,
+) -> Result<()> {
+    let shard = select_shard_for(&request, &config)?;
+    let mut backend_transport = connect_to_shard_with_udp(shard).await?;
+
+    backend_transport.send(request).await?;
+    let response = backend_transport
+        .next()
+        .await
+        .ok_or_else(|| anyhow!("Failed to receive response from shard"))?;
+
+    Ok(())
+}
+
+async fn connect_to_shard_with_udp(shard: &ShardConfig) -> Result<UdpFramed<Codec>> {
+    todo!();
 }
