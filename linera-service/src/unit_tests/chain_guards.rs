08a64de1bbd560a22f51295df1996e1a0b3e36a8
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-07-20 18:04:49 +0000

    Test if a chain guard can be obtained again later
    Check that obtaining a guard for the same chain at different moments
    work.

---
+++ linera-service/src/unit_tests/chain_guards.rs
@@ -0,0 +1,18 @@
+use super::ChainGuards;
+use futures::FutureExt;
+use linera_base::messages::ChainId;
+use std::time::Duration;
+use tokio::time::sleep;
+
+/// Test if a chain guard can be obtained again after it has been dropped.
+#[tokio::test(start_paused = true)]
+async fn guard_can_be_obtained_later_again() {
+    let chain_id = ChainId::root(0);
+    let mut guards = ChainGuards::default();
+    // Obtain the guard the first time and drop it immediately
+    let _ = guards.guard(chain_id).await;
+    // Wait before obtaining the guard again
+    sleep(Duration::from_secs(10)).await;
+    // It should be available immediately on the second time
+    assert!(guards.guard(chain_id).now_or_never().is_some());
+}
