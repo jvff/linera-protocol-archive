4304d0f2c24811c3d5415b3d1d6bc15874e01492
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-07-20 19:54:17 +0000

    Remove unnecessary sleep
    There's no need for it in the test because there's only one task.

--- linera-service/src/unit_tests/chain_guards.rs
+++ linera-service/src/unit_tests/chain_guards.rs
@@ -11,14 +11,12 @@ use std::{
 use tokio::{sync::Barrier, time::sleep};
 
 /// Test if a chain guard can be obtained again after it has been dropped.
-#[tokio::test(start_paused = true)]
+#[tokio::test]
 async fn guard_can_be_obtained_later_again() {
     let chain_id = ChainId::root(0);
     let mut guards = ChainGuards::default();
     // Obtain the guard the first time and drop it immediately
     let _ = guards.guard(chain_id).await;
-    // Wait before obtaining the guard again
-    sleep(Duration::from_secs(10)).await;
     // It should be available immediately on the second time
     assert!(guards.guard(chain_id).now_or_never().is_some());
 }
