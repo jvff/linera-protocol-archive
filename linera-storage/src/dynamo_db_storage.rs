7ba39340c68b992c0beb19843efe5290df2e6c13
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-07-27 18:53:23 +0000

    Create a `TableName` helper type
    Validate strings so that they represent valid DynamoDB table names.

---
+++ linera-storage/src/dynamo_db_storage.rs
@@ -0,0 +1,44 @@
+use linera_base::ensure;
+use std::str::FromStr;
+use thiserror::Error;
+
+/// A DynamoDB table name.
+///
+/// Table names must follow some [naming
+/// rules](https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/HowItWorks.NamingRulesDataTypes.html#HowItWorks.NamingRules),
+/// so this type ensures that they are properly validated.
+#[derive(Clone, Debug)]
+pub struct TableName(String);
+
+impl FromStr for TableName {
+    type Err = InvalidTableName;
+
+    fn from_str(string: &str) -> Result<Self, Self::Err> {
+        ensure!(string.len() >= 3, InvalidTableName::TooShort);
+        ensure!(string.len() <= 255, InvalidTableName::TooLong);
+        ensure!(
+            string
+                .chars()
+                .all(|character| character.is_ascii_alphanumeric()
+                    || character == '.'
+                    || character == '-'
+                    || character == '_'),
+            InvalidTableName::InvalidCharacter
+        );
+
+        Ok(TableName(string.to_owned()))
+    }
+}
+
+/// Error when validating a table name.
+#[derive(Debug, Error)]
+pub enum InvalidTableName {
+    #[error("Table name must have at least 3 characters")]
+    TooShort,
+
+    #[error("Table name must be at most 63 characters")]
+    TooLong,
+
+    #[error("Table name must only contain lowercase letters, numbers, periods and hyphens")]
+    InvalidCharacter,
+}
