055c53ffa1f6af89bbb8b2c69c58416a56a2c5a5
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-09-19 20:41:56 +0000

    fixup! Create `DynamoDbStoreClient` type

--- linera-storage2/src/dynamo_db.rs
+++ linera-storage2/src/dynamo_db.rs
@@ -28,44 +28,53 @@ struct DynamoDbStore {
 pub struct DynamoDbStoreClient(Arc<Mutex<DynamoDbStore>>);
 
 impl DynamoDbStoreClient {
-    pub async fn new(table: TableName) -> Result<Self, CreateTableError> {
-        Ok(DynamoDbStoreClient(Arc::new(Mutex::new(
-            DynamoDbStore::new(table).await?,
-        ))))
+    pub async fn new(table: TableName) -> Result<(Self, TableStatus), CreateTableError> {
+        Self::with_store(DynamoDbStore::new(table)).await
     }
 
     pub async fn from_config(
         config: impl Into<Config>,
         table: TableName,
-    ) -> Result<Self, CreateTableError> {
-        Ok(DynamoDbStoreClient(Arc::new(Mutex::new(
-            DynamoDbStore::from_config(config.into(), table).await?,
-        ))))
+    ) -> Result<(Self, TableStatus), CreateTableError> {
+        let (store, table_status) = DynamoDbStore::from_config(config.into(), table).await?;
+        let client = DynamoDbStoreClient(Arc::new(Mutex::new(store)));
+        Ok((client, table_status))
     }
 
-    pub async fn with_localstack(table: TableName) -> Result<Self, LocalStackError> {
-        Ok(DynamoDbStoreClient(Arc::new(Mutex::new(
-            DynamoDbStore::with_localstack(table).await?,
-        ))))
+    pub async fn with_localstack(table: TableName) -> Result<(Self, TableStatus), LocalStackError> {
+        let (store, table_status) = DynamoDbStore::with_localstack(table).await?;
+        let client = DynamoDbStoreClient(Arc::new(Mutex::new(store)));
+        Ok((client, table_status))
+    }
+
+    async fn with_store<E>(
+        store_creator: impl Future<Output = Result<(DynamoDbStore, TableStatus), E>>,
+    ) -> Result<(Self, TableStatus), E> {
+        let (store, table_status) = store_creator.await?;
+        let client = DynamoDbStoreClient(Arc::new(Mutex::new(store)));
+        Ok((client, table_status))
     }
 }
 
 impl DynamoDbStore {
-    pub async fn new(table: TableName) -> Result<Self, CreateTableError> {
+    pub async fn new(table: TableName) -> Result<(Self, TableStatus), CreateTableError> {
         Self::with_context(|lock, key_prefix, extra| {
             DynamoDbContext::new(table, lock, key_prefix, extra)
         })
         .await
     }
 
-    pub async fn from_config(config: Config, table: TableName) -> Result<Self, CreateTableError> {
+    pub async fn from_config(
+        config: Config,
+        table: TableName,
+    ) -> Result<(Self, TableStatus), CreateTableError> {
         Self::with_context(|lock, key_prefix, extra| {
             DynamoDbContext::from_config(config, table, lock, key_prefix, extra)
         })
         .await
     }
 
-    pub async fn with_localstack(table: TableName) -> Result<Self, LocalStackError> {
+    pub async fn with_localstack(table: TableName) -> Result<(Self, TableStatus), LocalStackError> {
         Self::with_context(|lock, key_prefix, extra| {
             DynamoDbContext::with_localstack(table, lock, key_prefix, extra)
         })
@@ -74,18 +83,21 @@ impl DynamoDbStore {
 
     async fn with_context<F, E>(
         create_context: impl FnOnce(OwnedMutexGuard<()>, Vec<u8>, ()) -> F,
-    ) -> Result<Self, E>
+    ) -> Result<(Self, TableStatus), E>
     where
         F: Future<Output = Result<(DynamoDbContext<()>, TableStatus), E>>,
     {
         let dummy_lock = Arc::new(Mutex::new(())).lock_owned().await;
         let empty_prefix = vec![];
         let dummy_extra = ();
-        let (context, _) = create_context(dummy_lock, empty_prefix, dummy_extra).await?;
-        Ok(Self {
-            context,
-            locks: HashMap::new(),
-        })
+        let (context, table_status) = create_context(dummy_lock, empty_prefix, dummy_extra).await?;
+        Ok((
+            Self {
+                context,
+                locks: HashMap::new(),
+            },
+            table_status,
+        ))
     }
 }
 
