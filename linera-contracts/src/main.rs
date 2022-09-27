53f9fb3ce4799c95a624adb25d5d77d2510a38d7
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-09-27 19:14:19 +0000

    Run Wasm module using Wasmer
    Execute the dummy example using a different runtime. Use a separate
    feature gate for the Wasmer runtime.

--- linera-contracts/src/main.rs
+++ linera-contracts/src/main.rs
@@ -1,5 +1,25 @@
+#[cfg(feature = "wasmer")]
+use wasmer::{imports, Instance, Module, Store, Value};
 #[cfg(feature = "wasmtime")]
 use wasmtime::{Engine, Instance, Module, Store};
+
+#[cfg(feature = "wasmer")]
+fn main() -> Result<(), anyhow::Error> {
+    let store = Store::default();
+    let module = Module::from_file(
+        &store,
+        "example/target/wasm32-unknown-unknown/debug/linera_contract_example.wasm",
+    )?;
+    let import_object = imports! {};
+    let instance = Instance::new(&module, &import_object)?;
+    let entry_point = instance.exports.get_function("example")?;
+
+    let result = entry_point.call(&[Value::I32(100)])?;
+
+    println!("{result:?}");
+
+    Ok(())
+}
 
 #[cfg(feature = "wasmtime")]
 fn main() -> Result<(), anyhow::Error> {
