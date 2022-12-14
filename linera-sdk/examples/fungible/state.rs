2b4c8ce06b1b1a787ba8180c30aa7aadd450806a
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2022-12-14 18:52:22 +0000

    Create `AccountOwner` type
    A type to identify accounts. An account is either owner by a secret key
    stored off-chain, or by a specific application.

---
+++ linera-sdk/examples/fungible/state.rs
@@ -0,0 +1,37 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use ed25519_dalek::PublicKey;
+use linera_sdk::ApplicationId;
+use serde::{Deserialize, Serialize};
+use std::cmp::Ordering;
+
+/// An account owner.
+#[derive(Clone, Copy, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub enum AccountOwner {
+    /// An account protected by a private key.
+    Key(PublicKey),
+    /// An account for an application.
+    Application(ApplicationId),
+}
+
+impl PartialOrd for AccountOwner {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for AccountOwner {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (AccountOwner::Key(_), AccountOwner::Application(_)) => Ordering::Less,
+            (AccountOwner::Application(_), AccountOwner::Key(_)) => Ordering::Greater,
+            (AccountOwner::Key(first), AccountOwner::Key(second)) => {
+                first.as_bytes().cmp(second.as_bytes())
+            }
+            (AccountOwner::Application(first), AccountOwner::Application(second)) => {
+                first.cmp(second)
+            }
+        }
+    }
+}
