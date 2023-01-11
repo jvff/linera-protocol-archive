b7e0146943fae02371db43f4f8a4ec862fe6631e
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-01-11 12:17:50 +0000

    Create initial test of application creation
    Prepare the test environment, creating a micro-chain that will publish
    the bytecode and a micro-chain that will create the application.

--- linera-core/src/unit_tests/worker_tests.rs
+++ linera-core/src/unit_tests/worker_tests.rs
@@ -3094,6 +3094,57 @@ where
 }
 
 #[test(tokio::test)]
+async fn test_memory_handle_certificates_to_create_application() -> Result<(), anyhow::Error> {
+    let client = MemoryStoreClient::default();
+    run_test_handle_certificates_to_create_application(client).await
+}
+
+#[test(tokio::test)]
+async fn test_rocksdb_handle_certificates_to_create_application() -> Result<(), anyhow::Error> {
+    let dir = tempfile::TempDir::new().unwrap();
+    let client = RocksdbStoreClient::new(dir.path().to_path_buf());
+    run_test_handle_certificates_to_create_application(client).await
+}
+
+#[test(tokio::test)]
+#[ignore]
+async fn test_dynamo_db_handle_certificates_to_create_application() -> Result<(), anyhow::Error> {
+    let table = "linera".parse().expect("Invalid table name");
+    let localstack = LocalStackTestContext::new().await?;
+    let (client, _) =
+        DynamoDbStoreClient::from_config(localstack.dynamo_db_config(), table).await?;
+    run_test_handle_certificates_to_create_application(client).await
+}
+
+async fn run_test_handle_certificates_to_create_application<S>(
+    client: S,
+) -> Result<(), anyhow::Error>
+where
+    S: Store + Clone + Send + Sync + 'static,
+    ViewError: From<S::ContextError>,
+{
+    let admin_id = ChainDescription::Root(0);
+    let publisher_key_pair = KeyPair::generate();
+    let publisher_chain = ChainDescription::Root(1);
+    let creator_key_pair = KeyPair::generate();
+    let creator_chain = ChainDescription::Root(2);
+    let (committee, mut worker) = init_worker_with_chains(
+        client,
+        vec![
+            (
+                publisher_chain,
+                publisher_key_pair.public(),
+                Balance::from(0),
+            ),
+            (creator_chain, creator_key_pair.public(), Balance::from(0)),
+        ],
+    )
+    .await;
+
+    todo!();
+}
+
+#[test(tokio::test)]
 async fn test_cross_chain_helper() {
     // Make a committee and worker (only used for signing certificates)
     let (committee, worker) = init_worker(MemoryStoreClient::default(), true);
