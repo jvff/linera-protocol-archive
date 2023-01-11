1d02bf9ccc911efb01977b739e460d0dc82fa0b9
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-01-11 12:56:50 +0000

    Only run test with a WASM runtime configured
    Don't run the test if no WASM runtime is enabled, because without a
    runtime the code will panic when creating an application.

--- linera-core/src/unit_tests/worker_tests.rs
+++ linera-core/src/unit_tests/worker_tests.rs
@@ -3094,12 +3094,14 @@ where
 }
 
 #[test(tokio::test)]
+#[cfg(any(feature = "wasmer", feature = "wasmtime"))]
 async fn test_memory_handle_certificates_to_create_application() -> Result<(), anyhow::Error> {
     let client = MemoryStoreClient::default();
     run_test_handle_certificates_to_create_application(client).await
 }
 
 #[test(tokio::test)]
+#[cfg(any(feature = "wasmer", feature = "wasmtime"))]
 async fn test_rocksdb_handle_certificates_to_create_application() -> Result<(), anyhow::Error> {
     let dir = tempfile::TempDir::new().unwrap();
     let client = RocksdbStoreClient::new(dir.path().to_path_buf());
@@ -3108,6 +3110,7 @@ async fn test_rocksdb_handle_certificates_to_create_application() -> Result<(), 
 
 #[test(tokio::test)]
 #[ignore]
+#[cfg(any(feature = "wasmer", feature = "wasmtime"))]
 async fn test_dynamo_db_handle_certificates_to_create_application() -> Result<(), anyhow::Error> {
     let table = "linera".parse().expect("Invalid table name");
     let localstack = LocalStackTestContext::new().await?;
@@ -3116,6 +3119,7 @@ async fn test_dynamo_db_handle_certificates_to_create_application() -> Result<()
     run_test_handle_certificates_to_create_application(client).await
 }
 
+#[cfg(any(feature = "wasmer", feature = "wasmtime"))]
 async fn run_test_handle_certificates_to_create_application<S>(
     client: S,
 ) -> Result<(), anyhow::Error>
