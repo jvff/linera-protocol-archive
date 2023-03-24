811049c7be1d569e8d060c1de102f85561735fa5
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2023-03-24 14:15:45 -0700

    Update linera-base/src/data_types.rs
    Co-authored-by: Andreas Fackler <afck@users.noreply.github.com>

--- linera-base/src/data_types.rs
+++ linera-base/src/data_types.rs
@@ -168,7 +168,7 @@ macro_rules! impl_strictly_wrapped_number {
 
         impl std::fmt::Display for $name {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                write!(f, "{}", self.0)
+                self.0.fmt(f)
             }
         }
 
