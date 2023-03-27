35005989c7af78bb709fde39a6861719d90e958b
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-03-27 14:20:07 +0000

    WIP

--- linera-sdk/src/extensions.rs
+++ linera-sdk/src/extensions.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use serde::de::DeserializeOwned;
+use serde::{de::DeserializeOwned, Serialize};
 
 pub trait FromBcsBytes: Sized {
     fn from_bcs_bytes(bytes: &[u8]) -> Result<Self, bcs::Error>;
@@ -13,5 +13,18 @@ where
 {
     fn from_bcs_bytes(bytes: &[u8]) -> Result<Self, bcs::Error> {
         bcs::from_bytes(bytes)
+    }
+}
+
+pub trait ToBcsBytes {
+    fn to_bcs_bytes(&self) -> Result<Vec<u8>, bcs::Error>;
+}
+
+impl<T> ToBcsBytes for T
+where
+    T: Serialize,
+{
+    fn to_bcs_bytes(&self) -> Result<Vec<u8>, bcs::Error> {
+        bcs::to_bytes(self)
     }
 }
