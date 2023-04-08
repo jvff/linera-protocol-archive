efcdeeba64a9da2a9fe9a7d1a7b73494987e6115
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-04-08 11:57:38 +0000

    Move integration test helpers to `integration`
    Prepare to include both integration test helpers and unit test helpers
    in the `test` module.

---
+++ linera-sdk/src/test/integration/mod.rs
@@ -0,0 +1,15 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Helper types for writing integration tests for WebAssembly applications.
+//!
+//! Integration tests are usually written in the `tests` directory in the root of the crate's
+//! directory (i.e., beside the `src` directory). Linera application integration tests should be
+//! executed targeting the host architecture, instead of targeting `wasm32-unknown-unknown` like
+//! done for unit tests.
+
+mod block;
+mod chain;
+mod validator;
+
+pub use self::{block::BlockBuilder, chain::ActiveChain, validator::TestValidator};
--- linera-sdk/src/test/mod.rs
+++ linera-sdk/src/test/mod.rs
@@ -1,20 +1,18 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-//! Helper types for writing integration tests for WebAssembly applications.
+//! Helper types for writing tests for WebAssembly applications.
 //!
-//! Integration tests are usually written in the `tests` directory in the root of the crate's
-//! directory (i.e., beside the `src` directory). Linera application integration tests should be
-//! executed targeting the host architecture, instead of targeting `wasm32-unknown-unknown` like
-//! done for unit tests.
+//! This module re-exports the types for either [`unit`] tests or [`integration`] tests.
+//!
+//! Unit tests are usually written in the `src` directory in the root of the crate's directory, and
+//! are executed targeting the `wasm32-unknown-unknown` target. Integration tests are usually
+//! written in the `tests` directory instead, and are executed targeting the host architecture.
 
-#![cfg(all(
-    not(target_arch = "wasm32"),
-    any(feature = "wasmer", feature = "wasmtime")
-))]
+#![cfg(any(feature = "wasmer", feature = "wasmtime"))]
 
-mod block;
-mod chain;
-mod validator;
+#[cfg(not(target_arch = "wasm32"))]
+mod integration;
 
-pub use self::{block::BlockBuilder, chain::ActiveChain, validator::TestValidator};
+#[cfg(not(target_arch = "wasm32"))]
+pub use self::integration::*;
