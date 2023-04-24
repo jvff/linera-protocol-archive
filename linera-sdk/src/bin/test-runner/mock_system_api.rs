69bf4270f52978c5596c9f81deec37773eba3024
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-04-24 20:34:18 +0000

    fixup! Proxy `application_id` getter system API

--- linera-sdk/src/bin/test-runner/mock_system_api.rs
+++ linera-sdk/src/bin/test-runner/mock_system_api.rs
@@ -373,12 +373,12 @@ pub fn add_to_linker(linker: &mut Linker<Resources>) -> Result<()> {
             bytecode-id: record { \
                 chain-id: record { part1: u64, part2: u64, part3: u64, part4: u64 }, \
                 height: u64, \
-                index: u64 \
+                index: u32 \
             }, \
             creation: record { \
                 chain-id: record { part1: u64, part2: u64, part3: u64, part4: u64 }, \
                 height: u64, \
-                index: u64 \
+                index: u32 \
             } \
         }",
         move |mut caller: Caller<'_, Resources>, return_offset: i32| {
@@ -389,12 +389,12 @@ pub fn add_to_linker(linker: &mut Linker<Resources>) -> Result<()> {
                         bytecode-id: record { \
                             chain-id: record { part1: u64, part2: u64, part3: u64, part4: u64 }, \
                             height: u64, \
-                            index: u64 \
+                            index: u32 \
                         }, \
                         creation: record { \
                             chain-id: record { part1: u64, part2: u64, part3: u64, part4: u64 }, \
                             height: u64, \
-                            index: u64 \
+                            index: u32 \
                         } \
                     }",
                 )
