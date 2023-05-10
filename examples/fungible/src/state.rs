50d032b2a5ed4e58feb8e5ba88b19e780701f01b
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-05-10 21:56:06 +0000

    Remove generics from views in examples
    Use the new syntax in the example applications.

--- examples/fungible/src/state.rs
+++ examples/fungible/src/state.rs
@@ -2,26 +2,18 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use fungible::{AccountOwner, InitialState};
-use linera_sdk::base::Amount;
-use linera_views::{
-    common::Context,
-    map_view::MapView,
-    views::{GraphQLView, RootView},
-};
+use linera_sdk::{base::Amount, views::MapView};
+use linera_views::views::{WasmGraphQLView, WasmRootView};
 use thiserror::Error;
 
 /// The application state.
-#[derive(RootView, GraphQLView)]
-pub struct FungibleToken<C> {
-    accounts: MapView<C, AccountOwner, Amount>,
+#[derive(WasmRootView, WasmGraphQLView)]
+pub struct FungibleToken {
+    accounts: MapView<AccountOwner, Amount>,
 }
 
 #[allow(dead_code)]
-impl<C> FungibleToken<C>
-where
-    C: Context + Send + Sync + Clone + 'static,
-    linera_views::views::ViewError: From<C::Error>,
-{
+impl FungibleToken {
     /// Initializes the application state with some accounts with initial balances.
 
     pub(crate) async fn initialize_accounts(&mut self, state: InitialState) {
