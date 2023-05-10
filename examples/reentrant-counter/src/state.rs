36978e5949b3a03efb58114474b89c5f603b5931
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-05-10 21:56:06 +0000

    Remove generics from views in examples
    Use the new syntax in the example applications.

--- examples/reentrant-counter/src/state.rs
+++ examples/reentrant-counter/src/state.rs
@@ -1,10 +1,11 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use linera_views::{register_view::RegisterView, views::RootView};
+use linera_sdk::views::RegisterView;
+use linera_views::views::WasmRootView;
 
 /// The application state.
-#[derive(Debug, RootView)]
-pub struct ReentrantCounter<C> {
-    pub value: RegisterView<C, u128>,
+#[derive(WasmRootView)]
+pub struct ReentrantCounter {
+    pub value: RegisterView<u128>,
 }
