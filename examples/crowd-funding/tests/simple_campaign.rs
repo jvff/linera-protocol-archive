701c94db0af99d221b255d2d046b5d70fdfc3727
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-05-30 19:19:40 +0000

    fixup! WIP Test a simple successful crowd-funding campaign

--- examples/crowd-funding/tests/simple_campaign.rs
+++ examples/crowd-funding/tests/simple_campaign.rs
@@ -12,13 +12,13 @@ use linera_sdk::{
     test::TestValidator,
 };
 
-/// Test transfering tokens across microchains.
+/// Test creating a campaign and collecting pledges.
 ///
-/// Creates the application on a `sender_chain`, initializing it with a single account with some
-/// tokens for that chain's owner. Transfers some of those tokens to a new `receiver_chain`, and
-/// checks that the balances on each microchain are correct.
+/// Creates a campaign on a `campaign_chain` and sets up the fungible token to use with three
+/// backer chains. Pledges part of each backer's balance to the campaign and then completes it,
+/// collecting the pledges. The final balance of each backer and the campaign owner is checked.
 #[tokio::test]
-async fn simple_successful_campaign() {
+async fn collect_pledges() {
     let initial_amount = Amount::from(100);
     let target_amount = Amount::from(220);
     let pledge_amount = Amount::from(75);
@@ -26,32 +26,19 @@ async fn simple_successful_campaign() {
     let (validator, bytecode_id) = TestValidator::with_current_bytecode().await;
 
     let fungible_publisher_chain = validator.new_chain().await;
-    let mut token_chain = validator.new_chain().await;
     let mut campaign_chain = validator.new_chain().await;
-    let backer1_chain = validator.new_chain().await;
-    let backer2_chain = validator.new_chain().await;
-    let backer3_chain = validator.new_chain().await;
-
     let campaign_account = AccountOwner::from(campaign_chain.public_key());
-    let backer1_account = AccountOwner::from(backer1_chain.public_key());
-    let backer2_account = AccountOwner::from(backer2_chain.public_key());
-    let backer3_account = AccountOwner::from(backer3_chain.public_key());
 
-    let initial_tokens = fungible::InitialStateBuilder::default()
-        .with_account(backer1_account, initial_amount)
-        .with_account(backer2_account, initial_amount)
-        .with_account(backer3_account, initial_amount);
     let fungible_bytecode_id = fungible_publisher_chain
         .publish_bytecodes_in("../fungible")
         .await;
-    let token_id = token_chain
-        .create_application::<FungibleTokenAbi>(
-            fungible_bytecode_id,
-            (),
-            initial_tokens.build(),
-            vec![],
-        )
-        .await;
+
+    let (token_id, backers) = FungibleTokenAbi::create_with_accounts(
+        &validator,
+        fungible_bytecode_id,
+        iter::repeat(initial_amount).take(3),
+    )
+    .await;
 
     let campaign_state = InitializationArgument {
         owner: campaign_account,
@@ -67,51 +54,32 @@ async fn simple_successful_campaign() {
         )
         .await;
 
-    let backers = [
-        (&backer1_chain, backer1_account),
-        (&backer2_chain, backer2_account),
-        (&backer3_chain, backer3_account),
-    ];
+    let mut pledges_and_transfers = Vec::with_capacity(2 * backers.len());
 
-    for (backer_chain, backer_account) in backers {
+    for (backer_chain, backer_account, _balance) in &backers {
         backer_chain.register_application(campaign_id).await;
 
-        backer_chain
-            .add_block(|block| {
-                block.with_operation(
-                    token_id,
-                    fungible::Operation::Claim {
-                        source_account: fungible::Account {
-                            chain_id: token_chain.id(),
-                            owner: backer_account,
-                        },
-                        amount: pledge_amount,
-                        target_account: fungible::Account {
-                            chain_id: backer_chain.id(),
-                            owner: backer_account,
-                        },
-                    },
-                );
-            })
-            .await;
-
-        token_chain.handle_received_effects().await;
-        backer_chain.handle_received_effects().await;
-
-        backer_chain
+        let pledge_effects = backer_chain
             .add_block(|block| {
                 block.with_operation(
                     campaign_id,
                     Operation::PledgeWithTransfer {
-                        owner: backer_account,
+                        owner: *backer_account,
                         amount: pledge_amount,
                     },
                 );
             })
             .await;
+
+        assert_eq!(pledge_effects.len(), 3);
+        pledges_and_transfers.extend(pledge_effects);
     }
 
-    campaign_chain.handle_received_effects().await;
+    campaign_chain
+        .add_block(|block| {
+            block.with_incoming_messages(pledges_and_transfers);
+        })
+        .await;
 
     assert_eq!(
         FungibleTokenAbi::query_account(token_id, &campaign_chain, campaign_account).await,
@@ -133,14 +101,10 @@ async fn simple_successful_campaign() {
         ),
     );
 
-    for (backer_chain, backer_account) in backers {
+    for (backer_chain, backer_account, initial_amount) in backers {
         assert_eq!(
-            FungibleTokenAbi::query_account(token_id, &token_chain, backer_account).await,
+            FungibleTokenAbi::query_account(token_id, &backer_chain, backer_account).await,
             Some(initial_amount.saturating_sub(pledge_amount)),
-        );
-        assert_eq!(
-            FungibleTokenAbi::query_account(token_id, backer_chain, backer_account).await,
-            Some(Amount::from(0)),
         );
         assert_eq!(
             FungibleTokenAbi::query_account(token_id, &campaign_chain, backer_account).await,
