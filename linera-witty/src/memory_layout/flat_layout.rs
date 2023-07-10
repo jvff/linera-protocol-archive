c74cc2313de399413fc42e7e3920fab3de3bf74f
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-10 18:31:16 +0000

    Remove `FlatAppend`

--- linera-witty/src/memory_layout/flat_layout.rs
+++ linera-witty/src/memory_layout/flat_layout.rs
@@ -4,7 +4,7 @@
 //! Representation of the layout of complex types as a sequence of native WebAssembly types.
 
 use super::Layout;
-use crate::{primitive_types::FlatType, util::Split};
+use crate::primitive_types::FlatType;
 use frunk::{HCons, HNil};
 
 /// Representation of the layout of complex types as a sequence of native WebAssembly types.
@@ -12,41 +12,13 @@ use frunk::{HCons, HNil};
 /// This allows laying out complex types as a sequence of WebAssembly types that can represent the
 /// parameters or the return list of a function. WIT uses this as an optimization to pass complex
 /// types as multiple native WebAssembly parameters.
-pub trait FlatLayout: Layout<Flat = Self> {
-    /// Result of appending some `Other` flat layout to this flat layout.
-    type FlatAppend<Other: FlatLayout>: FlatLayout + Split<Self, Remainder = Other>;
+pub trait FlatLayout: Layout<Flat = Self> {}
 
-    /// Appends some `other` flat layout with this flat layout, returning a new flat layout list.
-    fn flat_append<Other>(self, other: Other) -> Self::FlatAppend<Other>
-    where
-        Other: FlatLayout;
-}
-
-impl FlatLayout for HNil {
-    type FlatAppend<Other: FlatLayout> = Other;
-
-    fn flat_append<Other>(self, other: Other) -> Self::FlatAppend<Other>
-    where
-        Other: FlatLayout,
-    {
-        other
-    }
-}
+impl FlatLayout for HNil {}
 
 impl<Head, Tail> FlatLayout for HCons<Head, Tail>
 where
     Head: FlatType,
     Tail: FlatLayout,
 {
-    type FlatAppend<Other: FlatLayout> = HCons<Head, Tail::FlatAppend<Other>>;
-
-    fn flat_append<Other>(self, other: Other) -> Self::FlatAppend<Other>
-    where
-        Other: FlatLayout,
-    {
-        HCons {
-            head: self.head,
-            tail: self.tail.flat_append(other),
-        }
-    }
 }
