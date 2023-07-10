8a92f8cefbd1fa37d6f3fae6dfbd003e4992d46f
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-10 18:32:07 +0000

    Remove `Layout::Append`

--- linera-witty/src/memory_layout/layout.rs
+++ linera-witty/src/memory_layout/layout.rs
@@ -4,7 +4,7 @@
 //! Representation of the memory layout of complex types as a sequence of fundamental WIT types.
 
 use super::{element::LayoutElement, FlatLayout};
-use crate::{primitive_types::MaybeFlatType, util::Split};
+use crate::primitive_types::MaybeFlatType;
 use frunk::{hlist::HList, HCons, HNil};
 
 /// Marker trait to prevent [`LayoutElement`] to be implemented for other types.
@@ -18,18 +18,10 @@ pub trait Layout: Sealed + Default + HList {
     /// Result of flattening this layout.
     type Flat: FlatLayout;
 
-    /// Result of appending some `Other` layout to this layout.
-    type Append<Other: Layout>: Layout + Split<Self, Remainder = Other>;
-
     /// Flattens this layout into a layout consisting of native WebAssembly types.
     ///
     /// The resulting flat layout does not have any empty items.
     fn flatten(self) -> Self::Flat;
-
-    /// Appends some `other` layout with this layout, returning a new layout list.
-    fn append<Other>(self, other: Other) -> Self::Append<Other>
-    where
-        Other: Layout;
 }
 
 impl Sealed for HNil {}
@@ -44,17 +36,9 @@ impl Layout for HNil {
     const ALIGNMENT: u32 = 1;
 
     type Flat = HNil;
-    type Append<Other: Layout> = Other;
 
     fn flatten(self) -> Self::Flat {
         HNil
-    }
-
-    fn append<Other>(self, other: Other) -> Self::Append<Other>
-    where
-        Other: Layout,
-    {
-        other
     }
 }
 
@@ -70,19 +54,8 @@ where
     };
 
     type Flat = <Head::Flat as MaybeFlatType>::Flatten<Tail>;
-    type Append<Other: Layout> = HCons<Head, Tail::Append<Other>>;
 
     fn flatten(self) -> Self::Flat {
         self.head.flatten().flatten(self.tail)
-    }
-
-    fn append<Other>(self, other: Other) -> Self::Append<Other>
-    where
-        Other: Layout,
-    {
-        HCons {
-            head: self.head,
-            tail: self.tail.append(other),
-        }
     }
 }
