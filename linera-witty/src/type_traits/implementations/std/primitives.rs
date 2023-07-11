1ef9fe648d1560824cdd6d7d153ecd1dfde41707
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-11 22:22:25 +0000

    Implement `WitType` for some standard lib. types
    Implement `WitType` for some initial types from the standard library.
    The types were selected so that they could allow some custom Linera
    types to derive `WitType`.

---
+++ linera-witty/src/type_traits/implementations/std/primitives.rs
@@ -0,0 +1,28 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Implementations of the custom traits for other standard primitive types.
+
+use crate::WitType;
+use frunk::{HList, HNil};
+
+impl WitType for () {
+    const SIZE: u32 = 0;
+
+    type Layout = HNil;
+}
+
+impl WitType for bool {
+    const SIZE: u32 = 1;
+
+    type Layout = HList![i8];
+}
+
+impl<'t, T> WitType for &'t T
+where
+    T: WitType,
+{
+    const SIZE: u32 = T::SIZE;
+
+    type Layout = T::Layout;
+}
