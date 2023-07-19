e536991b8ad310c7effd515abc43ba466cd7d5f0
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-19 23:54:12 +0000

    Implement `WitType` for `GuestPointer`
    Prepare to make it simple to load guest memory pointers.

---
+++ linera-witty/src/type_traits/implementations/custom_types.rs
@@ -0,0 +1,15 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Implementations of the custom traits for types declared in this crate.
+
+use {
+    crate::{GuestPointer, WitType},
+    frunk::HList,
+};
+
+impl WitType for GuestPointer {
+    const SIZE: u32 = u32::SIZE;
+
+    type Layout = HList![i32];
+}
