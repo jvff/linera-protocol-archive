78c490380f7d1d33e76509d960337f9867644595
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-26 01:05:15 +0000

    Test the derive macro for `WitStore`
    Ensure the generated code can store the types in simulated memory and
    lower them into their flat layouts.

--- linera-witty/tests/common/types.rs
+++ linera-witty/tests/common/types.rs
@@ -3,23 +3,23 @@
 
 //! Dummy types used in tests.
 
-use linera_witty::{WitLoad, WitType};
+use linera_witty::{WitLoad, WitStore, WitType};
 
 /// A type that wraps a simple type.
-#[derive(Clone, Copy, Debug, Eq, PartialEq, WitType, WitLoad)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, WitType, WitLoad, WitStore)]
 pub struct SimpleWrapper(pub bool);
 
 /// A tuple struct that doesn't need any internal padding in its memory layout.
-#[derive(Clone, Copy, Debug, Eq, PartialEq, WitType, WitLoad)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, WitType, WitLoad, WitStore)]
 pub struct TupleWithoutPadding(pub u64, pub i32, pub i16);
 
 /// A tuple struct that requires internal padding in its memory layout between all of its fields.
-#[derive(Clone, Copy, Debug, Eq, PartialEq, WitType, WitLoad)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, WitType, WitLoad, WitStore)]
 pub struct TupleWithPadding(pub u16, pub u32, pub i64);
 
 /// A struct with named fields that requires padding in two locations in its memory layout.
 #[allow(dead_code)]
-#[derive(Clone, Copy, Debug, Eq, PartialEq, WitType, WitLoad)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, WitType, WitLoad, WitStore)]
 pub struct RecordWithDoublePadding {
     pub first: u16,
     pub second: u32,
@@ -29,7 +29,7 @@ pub struct RecordWithDoublePadding {
 
 /// A simple struct with named fields to be used inside a more complex struct.
 #[allow(dead_code)]
-#[derive(Clone, Copy, Debug, Eq, PartialEq, WitType, WitLoad)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, WitType, WitLoad, WitStore)]
 pub struct Leaf {
     pub first: bool,
     pub second: u128,
@@ -37,7 +37,7 @@ pub struct Leaf {
 
 /// A struct that contains fields with custom types that also have derived trait implementations.
 #[allow(dead_code)]
-#[derive(Clone, Copy, Debug, Eq, PartialEq, WitType, WitLoad)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, WitType, WitLoad, WitStore)]
 pub struct Branch {
     pub tag: u16,
     pub first_leaf: Leaf,
