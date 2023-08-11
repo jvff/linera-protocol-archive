136f8d1da32871234b5b55e4d071127668eaccb1
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-08-11 14:28:20 +0000

    Add some helper Wasm modules for testing
    Some simple Wasm modules that will be used during some integration
    tests.

---
+++ linera-witty/test-modules/src/export/simple_function.rs
@@ -0,0 +1,18 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Helper Wasm module with a minimal function without parameters or return values.
+
+#![no_main]
+
+wit_bindgen::generate!("export-simple-function");
+
+export_export_simple_function!(Implementation);
+
+use self::exports::witty_macros::test_modules::simple_function::SimpleFunction;
+
+struct Implementation;
+
+impl SimpleFunction for Implementation {
+    fn simple() {}
+}
