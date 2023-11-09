d6d2c9cd4a2e5c3591ea8bf3b4c9c3706a0b0f2b
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-11-09 01:06:40 +0000

    Make `handle_query` entrypoint synchronous
    Replace the asynchronous entrypoint.

--- linera-sdk/src/service/mod.rs
+++ linera-sdk/src/service/mod.rs
@@ -9,6 +9,14 @@ pub mod exported_futures;
 pub mod system_api;
 pub mod wit_types;
 
+use crate::ServiceLogger;
+use futures::task;
+use std::{
+    future::Future,
+    pin::pin,
+    task::{Context, Poll},
+};
+
 // Import the system interface.
 wit_bindgen_guest_rust::import!("service_system_api.wit");
 
@@ -25,14 +33,17 @@ macro_rules! service {
 
         /// Marks the service type to be exported.
         impl $crate::service::wit_types::Service for $application {
-            type HandleQuery = HandleQuery;
-        }
-
-        $crate::instance_exported_future! {
-            service::HandleQuery<$application>(
+            fn handle_query(
                 context: $crate::service::wit_types::QueryContext,
                 argument: Vec<u8>,
-            ) -> PollApplicationQueryResult
+            ) -> Result<Vec<u8>, String> {
+                $crate::service::run_async_entrypoint(
+                            <
+                                <$application as $crate::Service>::Storage
+                                    as $crate::service::exported_futures::ServiceStateStorage
+                            >::handle_query(context, argument),
+                        )
+            }
         }
 
         /// Stub of a `main` entrypoint so that the binary doesn't fail to compile on targets other
@@ -40,4 +51,29 @@ macro_rules! service {
         #[cfg(not(target_arch = "wasm32"))]
         fn main() {}
     };
+}
+
+/// Runs an asynchronous entrypoint in a blocking manner, by repeatedly polling the entrypoint
+/// future.
+pub fn run_async_entrypoint<Entrypoint, Output, Error, RawOutput>(
+    entrypoint: Entrypoint,
+) -> Result<RawOutput, String>
+where
+    Entrypoint: Future<Output = Result<Output, Error>> + Send,
+    Output: Into<RawOutput> + Send + 'static,
+    Error: ToString + 'static,
+{
+    ServiceLogger::install();
+
+    let waker = task::noop_waker();
+    let mut task_context = Context::from_waker(&waker);
+    let mut future = pin!(entrypoint);
+
+    loop {
+        match future.as_mut().poll(&mut task_context) {
+            Poll::Pending => continue,
+            Poll::Ready(Ok(output)) => return Ok(output.into()),
+            Poll::Ready(Err(error)) => return Err(error.to_string()),
+        }
+    }
 }
