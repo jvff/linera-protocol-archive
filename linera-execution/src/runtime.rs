2978330bb7f2585fceaf0f7f1da73cf6985a7e96
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2023-12-08 11:18:12 +0000

    Pop app. call stack before propagating errors
    Avoid reaching a situation where an assertion fails because the stack
    was not updated when propagating an error, but the error is then
    ignored.

--- linera-execution/src/runtime.rs
+++ linera-execution/src/runtime.rs
@@ -536,11 +536,12 @@ where
         let value_future = tokio::task::spawn_blocking(move || {
             code.handle_query(query_context, runtime_sender, argument)
         });
-        runtime_actor.run().await?;
-        let value = value_future.await??;
-
+        let runtime_result = runtime_actor.run().await;
+        let value = value_future.await;
         self.applications_mut().pop();
-        Ok(value)
+
+        runtime_result?;
+        value?
     }
 }
 
@@ -683,10 +684,12 @@ where
                 forwarded_sessions,
             )
         });
-        runtime_actor.run().await?;
-        let raw_result = raw_result_future.await??;
-
+        let runtime_result = runtime_actor.run().await;
+        let raw_result = raw_result_future.await;
         self.applications_mut().pop();
+        runtime_result?;
+        let raw_result = raw_result??;
+
         // Interpret the results of the call.
         self.execution_results_mut().push(ExecutionResult::User(
             callee_id,
@@ -749,10 +752,12 @@ where
                 forwarded_sessions,
             )
         });
-        runtime_actor.run().await?;
-        let (raw_result, session_state) = raw_result_future.await??;
-
+        let runtime_result = runtime_actor.run().await;
+        let raw_result = raw_result_future.await;
         self.applications_mut().pop();
+        runtime_result?;
+        let (raw_result, session_state) = raw_result??;
+
         // Interpret the results of the call.
         if raw_result.close_session {
             // Terminate the session.
