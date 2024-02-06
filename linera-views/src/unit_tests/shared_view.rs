6d78775a18692c92af91f999c745fed30565f123
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-02-06 21:00:08 +0000

    fixup! Test sharing a view among readers

--- linera-views/src/unit_tests/shared_view.rs
+++ linera-views/src/unit_tests/shared_view.rs
@@ -31,7 +31,7 @@ where
     let context = create_memory_context();
 
     let mut view = V::load(context).await?;
-    let staged_value = view.stage_initial_changes().await?;
+    let staged_state = view.stage_initial_changes().await?;
 
     let mut shared_view = SharedView::new(view);
 
@@ -53,10 +53,10 @@ where
 
     tasks
         .for_each_concurrent(100, |result| async {
-            let read_value = result
+            let read_state = result
                 .expect("Read task should not panic")
                 .expect("Reading through read-only view reference should not fail");
-            assert_eq!(read_value, staged_value);
+            assert_eq!(read_state, staged_state);
         })
         .await;
 
