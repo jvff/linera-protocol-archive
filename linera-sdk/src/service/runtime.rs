0ee54a4b47a941db491e1aa46b7df9b0a91058a0
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-03-04 14:05:03 +0000

    Create a `ServiceRuntime` type
    Serves as a proxy for the system APIs for a service, while caching
    responses that are known to be immutable during the execution.

---
+++ linera-sdk/src/service/runtime.rs
@@ -0,0 +1,28 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Runtime types to interface with the host executing the service.
+
+use super::service_system_api as wit;
+use linera_base::{data_types::BlockHeight, identifiers::ChainId};
+
+/// The runtime available during execution of a query.
+#[derive(Clone, Debug, Default)]
+pub struct ServiceRuntime {
+    chain_id: Option<ChainId>,
+    next_block_height: Option<BlockHeight>,
+}
+
+impl ServiceRuntime {
+    /// Returns the ID of the current chain.
+    pub fn chain_id(&mut self) -> ChainId {
+        *self.chain_id.get_or_insert_with(|| wit::chain_id().into())
+    }
+
+    /// Returns the height of the next block that can be added to the current chain.
+    pub fn next_block_height(&mut self) -> BlockHeight {
+        *self
+            .next_block_height
+            .get_or_insert_with(|| wit::next_block_height().into())
+    }
+}
