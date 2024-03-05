51888edd6d883494e48d896fce0a4f38238f4b33
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-03-05 14:35:05 +0000

    fixup! Create a `CalleeRuntime` type

--- linera-sdk/src/contract/mod.rs
+++ linera-sdk/src/contract/mod.rs
@@ -11,7 +11,7 @@ pub mod system_api;
 pub mod wit_types;
 
 pub use self::{
-    runtime::{CalleeRuntime, MessageRuntime, OperationRuntime, Runtime},
+    runtime::{MessageRuntime, OperationRuntime, Runtime},
     storage::ContractStateStorage,
 };
 use super::log::ContractLogger;
