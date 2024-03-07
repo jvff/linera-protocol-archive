4cc3a92f103ef1a46a7c4ac5747cb364d50e6a11
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-03-07 00:25:49 +0000

    Don't use `async_trait` for `Service` trait
    Remove `Send` requirement for the returned futures.

--- examples/counter/src/service.rs
+++ examples/counter/src/service.rs
@@ -7,7 +7,6 @@ mod state;
 
 use self::state::Counter;
 use async_graphql::{EmptySubscription, Object, Request, Response, Schema};
-use async_trait::async_trait;
 use counter::CounterAbi as Abi;
 use linera_sdk::{base::WithServiceAbi, Service, ServiceRuntime, SimpleStateStorage};
 use std::sync::Arc;
@@ -19,7 +18,6 @@ impl WithServiceAbi for Counter {
     type Abi = Abi;
 }
 
-#[async_trait]
 impl Service for Counter {
     type Error = Error;
     type Storage = SimpleStateStorage<Self>;
