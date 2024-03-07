d7110fe8378c1eef333c98b42de24b1349fc99f4
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-03-07 12:42:33 +0000

    Use `ServiceRuntime::owner_balance` method
    Replace usage of the equivalent `system_api`.

--- examples/native-fungible/src/service.rs
+++ examples/native-fungible/src/service.rs
@@ -6,22 +6,20 @@
 mod state;
 
 use self::state::NativeFungibleToken;
-use async_graphql::{
-    ComplexObject, EmptySubscription, Object, Request, Response, Schema, SimpleObject,
-};
+use async_graphql::{EmptySubscription, Object, Request, Response, Schema, SimpleObject};
 use fungible::Operation;
 use linera_sdk::{
     base::{AccountOwner, Amount, WithServiceAbi},
     graphql::GraphQLMutationRoot,
-    service::system_api,
     Service, ServiceRuntime, ViewStateStorage,
 };
 use native_fungible::TICKER_SYMBOL;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use thiserror::Error;
 
+#[derive(Clone)]
 pub struct NativeFungibleTokenService {
-    state: Arc<NativeFungibleToken>,
+    runtime: Arc<Mutex<ServiceRuntime<fungible::FungibleTokenAbi>>>,
 }
 
 linera_sdk::service!(NativeFungibleTokenService);
@@ -36,21 +34,17 @@ impl Service for NativeFungibleTokenService {
     type State = NativeFungibleToken;
 
     async fn new(
-        state: Self::State,
-        _runtime: ServiceRuntime<Self::Abi>,
+        _state: Self::State,
+        runtime: ServiceRuntime<Self::Abi>,
     ) -> Result<Self, Self::Error> {
         Ok(NativeFungibleTokenService {
-            state: Arc::new(state),
+            runtime: Arc::new(Mutex::new(runtime)),
         })
     }
 
     async fn handle_query(&self, request: Request) -> Result<Response, Self::Error> {
-        let schema = Schema::build(
-            self.state.clone(),
-            Operation::mutation_root(),
-            EmptySubscription,
-        )
-        .finish();
+        let schema =
+            Schema::build(self.clone(), Operation::mutation_root(), EmptySubscription).finish();
         let response = schema.execute(request).await;
         Ok(response)
     }
@@ -61,8 +55,9 @@ struct AccountEntry {
     value: Amount,
 }
 
-#[derive(Default)]
-struct Accounts;
+struct Accounts {
+    runtime: Arc<Mutex<ServiceRuntime<fungible::FungibleTokenAbi>>>,
+}
 
 #[Object]
 impl Accounts {
@@ -73,20 +68,26 @@ impl Accounts {
             AccountOwner::Application(_) => return Err(Error::ApplicationsNotSupported),
         };
 
-        let balance = system_api::current_owner_balance(owner);
+        let runtime = self
+            .runtime
+            .try_lock()
+            .expect("Services only run in a single thread");
+        let balance = runtime.owner_balance(owner);
         Ok(AccountEntry { value: balance })
     }
 }
 
 // Implements additional fields not derived from struct members of FungibleToken.
-#[ComplexObject]
-impl NativeFungibleToken {
+#[Object]
+impl NativeFungibleTokenService {
     async fn ticker_symbol(&self) -> Result<String, async_graphql::Error> {
         Ok(String::from(TICKER_SYMBOL))
     }
 
     async fn accounts(&self) -> Result<Accounts, async_graphql::Error> {
-        Ok(Accounts)
+        Ok(Accounts {
+            runtime: self.runtime.clone(),
+        })
     }
 }
 
