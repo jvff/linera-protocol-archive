90529ee4d64d065f94d293a7675078bf674ca41f
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-03-07 02:32:28 +0000

    Add a `ServiceRuntime::application_id` method
    Allow retrieving the application ID without using the `system_api`
    module.

--- linera-sdk/src/service/runtime.rs
+++ linera-sdk/src/service/runtime.rs
@@ -4,7 +4,11 @@
 //! Runtime types to interface with the host executing the service.
 
 use super::service_system_api as wit;
-use linera_base::{abi::ServiceAbi, data_types::BlockHeight, identifiers::ChainId};
+use linera_base::{
+    abi::ServiceAbi,
+    data_types::BlockHeight,
+    identifiers::{ApplicationId, ChainId},
+};
 use std::cell::Cell;
 
 /// The runtime available during execution of a query.
@@ -13,6 +17,7 @@ where
     Abi: ServiceAbi,
 {
     application_parameters: Cell<Option<Abi::Parameters>>,
+    application_id: Cell<Option<ApplicationId<Abi>>>,
     chain_id: Cell<Option<ChainId>>,
     next_block_height: Cell<Option<BlockHeight>>,
 }
@@ -25,6 +30,7 @@ where
     pub(crate) fn new() -> Self {
         ServiceRuntime {
             application_parameters: Cell::new(None),
+            application_id: Cell::new(None),
             chain_id: Cell::new(None),
             next_block_height: Cell::new(None),
         }
@@ -35,6 +41,13 @@ where
         Self::fetch_value_through_cache(&self.application_parameters, || {
             let bytes = wit::application_parameters();
             serde_json::from_slice(&bytes).expect("Application parameters must be deserializable")
+        })
+    }
+
+    /// Returns the ID of the current application.
+    pub fn application_id(&self) -> ApplicationId<Abi> {
+        Self::fetch_value_through_cache(&self.application_id, || {
+            ApplicationId::from(wit::application_id()).with_abi()
         })
     }
 
