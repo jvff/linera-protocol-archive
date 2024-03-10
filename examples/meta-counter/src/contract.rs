d7ed42e7ee7070e9f94031bb43c94cf12451afc2
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-03-10 02:19:37 +0000

    Remove `runtime` parameter from `Contract` methods
    The type can be obtained with a default constructor and should be stored
    in the contract type instead.

--- examples/meta-counter/src/contract.rs
+++ examples/meta-counter/src/contract.rs
@@ -51,7 +51,6 @@ impl Contract for MetaCounterContract {
 
     async fn initialize(
         &mut self,
-        _runtime: &mut ContractRuntime,
         _argument: (),
     ) -> Result<ExecutionOutcome<Self::Message>, Self::Error> {
         // Validate that the application parameters were configured correctly.
@@ -68,7 +67,6 @@ impl Contract for MetaCounterContract {
 
     async fn execute_operation(
         &mut self,
-        _runtime: &mut ContractRuntime,
         operation: Operation,
     ) -> Result<ExecutionOutcome<Self::Message>, Self::Error> {
         log::trace!("operation: {:?}", operation);
@@ -96,7 +94,6 @@ impl Contract for MetaCounterContract {
 
     async fn execute_message(
         &mut self,
-        _runtime: &mut ContractRuntime,
         message: Message,
     ) -> Result<ExecutionOutcome<Self::Message>, Self::Error> {
         let is_bouncing = self
@@ -122,7 +119,6 @@ impl Contract for MetaCounterContract {
 
     async fn handle_application_call(
         &mut self,
-        _runtime: &mut ContractRuntime,
         _call: (),
     ) -> Result<ApplicationCallOutcome<Self::Message, Self::Response>, Self::Error> {
         Err(Error::CallsNotSupported)
