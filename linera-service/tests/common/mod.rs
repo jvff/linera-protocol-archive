3731a58c28651722dc7d52acfadb4d889b0f6884
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-04-06 19:34:39 +0000

    Add a `take_from_json_value` test helper function
    Deserializes a `serde_json::Value` without having to clone the converted
    value.

--- linera-service/tests/common/mod.rs
+++ linera-service/tests/common/mod.rs
@@ -5,7 +5,11 @@
 
 mod node_service;
 
+use std::mem;
+
+use anyhow::Result;
 use linera_base::sync::Lazy;
+use serde::de::DeserializeOwned;
 use tokio::sync::Mutex;
 
 #[allow(unused_imports)]
@@ -13,3 +17,13 @@ pub use self::node_service::NodeService;
 
 /// A static lock to prevent integration tests from running in parallel.
 pub static INTEGRATION_TEST_GUARD: Lazy<Mutex<()>> = Lazy::new(|| Mutex::new(()));
+
+/// Takes the contents of a [`serde_json::Value`] and converts it into an instance of generic type `T`.
+///
+/// The `value` will contain [`serde_json::Value::Null`] afterwards.
+pub fn take_from_json_value<T>(value: &mut serde_json::Value) -> Result<T>
+where
+    T: DeserializeOwned,
+{
+    Ok(serde_json::from_value(mem::take(value))?)
+}
