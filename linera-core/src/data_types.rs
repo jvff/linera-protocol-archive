3053b11593683e07af1c094ab9ce034fcfc2e3ea
Andreas Fackler <andreas.fackler@linera.io>
2024-04-10 15:51:16 +0200

    Rename LeaderTimeout to Timeout.

--- linera-core/src/data_types.rs
+++ linera-core/src/data_types.rs
@@ -64,7 +64,7 @@ pub struct ChainInfoQuery {
     /// Query values from the chain manager, not just votes.
     pub request_manager_values: bool,
     /// Include a timeout vote for the current round, if appropriate.
-    pub request_leader_timeout: bool,
+    pub request_timeout: bool,
     /// Include a vote to switch to fallback mode, if appropriate.
     pub request_fallback: bool,
     /// Query a value that contains a binary blob (e.g. bytecode) required by this chain.
@@ -82,7 +82,7 @@ impl ChainInfoQuery {
             request_sent_certificates_in_range: None,
             request_received_log_excluding_first_nth: None,
             request_manager_values: false,
-            request_leader_timeout: false,
+            request_timeout: false,
             request_fallback: false,
             request_blob: None,
         }
@@ -123,8 +123,8 @@ impl ChainInfoQuery {
         self
     }
 
-    pub fn with_leader_timeout(mut self) -> Self {
-        self.request_leader_timeout = true;
+    pub fn with_timeout(mut self) -> Self {
+        self.request_timeout = true;
         self
     }
 
