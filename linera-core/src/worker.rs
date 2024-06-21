dcfb6efb6a364676bbd901f3eb78444bdaa20aab
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-06-21 12:18:16 +0000

    Revert "Revert "Add storage constraints to `WorkerState` type""
    This reverts commit 417075738d6d682900c029087f4311af9a3637a5.

--- linera-core/src/worker.rs
+++ linera-core/src/worker.rs
@@ -277,7 +277,11 @@ impl From<linera_chain::ChainError> for WorkerError {
 
 /// State of a worker in a validator or a local node.
 #[derive(Clone)]
-pub struct WorkerState<StorageClient> {
+pub struct WorkerState<StorageClient>
+where
+    StorageClient: Storage,
+    ViewError: From<StorageClient::ContextError>,
+{
     /// A name used for logging
     nickname: String,
     /// Access to local persistent storage.
@@ -304,7 +308,11 @@ type ChainActorEndpoint<StorageClient> =
 pub(crate) type DeliveryNotifiers =
     HashMap<ChainId, BTreeMap<BlockHeight, Vec<oneshot::Sender<()>>>>;
 
-impl<StorageClient> WorkerState<StorageClient> {
+impl<StorageClient> WorkerState<StorageClient>
+where
+    StorageClient: Storage,
+    ViewError: From<StorageClient::ContextError>,
+{
     pub fn new(nickname: String, key_pair: Option<KeyPair>, storage: StorageClient) -> Self {
         WorkerState {
             nickname,
@@ -778,7 +786,11 @@ where
 }
 
 #[cfg(with_testing)]
-impl<StorageClient> WorkerState<StorageClient> {
+impl<StorageClient> WorkerState<StorageClient>
+where
+    StorageClient: Storage,
+    ViewError: From<StorageClient::ContextError>,
+{
     /// Gets a reference to the validator's [`PublicKey`].
     ///
     /// # Panics
