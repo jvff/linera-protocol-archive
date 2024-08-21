0a73c366d4b6db08b3ed345741dac20deaee4108
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-08-21 14:39:28 +0000

    fixup! Add `Client::track_chain` method

--- linera-core/src/client.rs
+++ linera-core/src/client.rs
@@ -145,8 +145,6 @@ where
     }
 
     /// Adds a chain to the set of chains tracked by the local node.
-    ///
-    /// This only affects the [`ChainClient`]s created after this call.
     pub fn track_chain(&self, chain_id: ChainId) {
         self.tracked_chains
             .write()
