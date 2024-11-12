c136d10b4be979972f1914903c56eb75b454b910
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-11-12 21:27:36 +0000

    fixup! Add `max_active_chains` field to `Client`

--- linera-client/src/client_context.rs
+++ linera-client/src/client_context.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #[cfg(with_testing)]
-use std::num::NonZero;
+use std::num::NonZeroUsize;
 use std::{
     collections::{BTreeMap, HashSet},
     sync::Arc,
