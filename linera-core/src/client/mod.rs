1450586a427dc74724d0ae9b2eeddfa7946a3e08
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-11-18 18:15:15 +0000

    Use `AccountOwner` in `ChainInfoRequest`
    Allow requesting the balance of an application's account.

--- linera-core/src/client/mod.rs
+++ linera-core/src/client/mod.rs
@@ -33,8 +33,8 @@ use linera_base::{
     },
     ensure,
     identifiers::{
-        Account, ApplicationId, BlobId, BlobType, BytecodeId, ChainId, MessageId, Owner,
-        UserApplicationId,
+        Account, AccountOwner, ApplicationId, BlobId, BlobType, BytecodeId, ChainId, MessageId,
+        Owner, UserApplicationId,
     },
     ownership::{ChainOwnership, TimeoutConfig},
 };
@@ -2368,7 +2368,7 @@ where
             ChainClientError::WalletSynchronizationError
         );
         let mut query = ChainInfoQuery::new(self.chain_id);
-        query.request_owner_balance = owner;
+        query.request_owner_balance = owner.map(AccountOwner::User);
         let response = self
             .client
             .local_node
